15960afd0e7ffbc19035de27ccf7390147e12ccf
Arwa Alattas <arwaalattas@MacBook-Pro-Arwa.local>
2021-10-18 18:36:04 +0300

    slidercolor

--- SlaiderColor/SlaiderColor/ViewController.swift
+++ SlaiderColor/SlaiderColor/ViewController.swift
@@ -20,7 +20,9 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var blueSwitch: UISwitch!
-    
+    var redValue = 0.0
+     var greenValue = 0.0
+     var  blueValue = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,66 +30,105 @@ class ViewController: UIViewController {
     }
     
     @IBAction func Reast(_ sender: UIButton) {
-        
-        RedSwicth.isOn = true
-        greenSwitch.isOn = true
+       
         blueSwitch.isOn = true
+        greenSwitch.isOn = true
+        RedSwicth.isOn = true
         Slider1.isEnabled = true
         Slider2.isEnabled = true
         Slider3.isEnabled = true
-        Slider1.value = 0
-        Slider2.value = 0
-        Slider3.value = 0
+        Slider1.value = 0.0
+        Slider2.value = 0.0
+        Slider3.value = 0.0
+        ViewColor.backgroundColor = UIColor.black
         
     }
     
     @IBAction func Slider1Action(_ sender: UISlider) {
-        let redValue = CGFloat(Slider1.value)
-        let greenValue = CGFloat(Slider2.value)
-        let blueValue = CGFloat(Slider3.value)
+       
+        if RedSwicth.isOn{
+           redValue = CGFloat(Slider1.value)
+        }
+        if greenSwitch.isOn {
+            greenValue = CGFloat(Slider2.value)
+        }
+        if blueSwitch.isOn {
+             blueValue = CGFloat(Slider3.value)
+            
+        }
         
         ViewColor.backgroundColor = UIColor(red: redValue, green: greenValue ,blue: blueValue, alpha: 1 )
     }
 
     @IBAction func Slider2Action(_ sender: UISlider) {
-        let redValue = CGFloat(Slider1.value)
-        let greenValue = CGFloat(Slider2.value)
-        let blueValue = CGFloat(Slider3.value)
-        
-        ViewColor.backgroundColor = UIColor(red: redValue, green: greenValue ,blue: blueValue, alpha: 1 )
+       
+         if RedSwicth.isOn{
+            redValue = CGFloat(Slider1.value)
+         }
+         if greenSwitch.isOn {
+             greenValue = CGFloat(Slider2.value)
+         }
+         if blueSwitch.isOn {
+              blueValue = CGFloat(Slider3.value)
+             
+         }
+         
+         ViewColor.backgroundColor = UIColor(red: redValue, green: greenValue ,blue: blueValue, alpha: 1 )
+     }
 
-    }
     
    
     
     @IBAction func Slider3Action(_ sender: UISlider) {
-        let redValue = CGFloat(Slider1.value)
-        let greenValue = CGFloat(Slider2.value)
-        let blueValue = CGFloat(Slider3.value)
-        
-        ViewColor.backgroundColor = UIColor(red: redValue, green: greenValue ,blue: blueValue, alpha: 1 )
+       
+         if RedSwicth.isOn{
+            redValue = CGFloat(Slider1.value)
+         }
+         if greenSwitch.isOn {
+             greenValue = CGFloat(Slider2.value)
+         }
+         if blueSwitch.isOn {
+              blueValue = CGFloat(Slider3.value)
+             
+         }
+         
+         ViewColor.backgroundColor = UIColor(red: redValue, green: greenValue ,blue: blueValue, alpha: 1 )
+     }
 
         
-        
-    }
-        
-        
     @IBAction func redSwich(_ sender: UISwitch) {
-        
-        Slider1.isEnabled = false
+        if RedSwicth.isOn{
+        Slider1.isEnabled = true
         Slider1.value = 0
+        ViewColor.backgroundColor = UIColor .black
+        }
+        else{
+            Slider1.value = 0
+            Slider1.isEnabled = false
+        }
+        
     }
   
     @IBAction func greenSwitch(_ sender: UISwitch) {
-        Slider2.isEnabled = false
+        if greenSwitch.isOn{
+        Slider2.isEnabled = true
         Slider2.value = 0
-    }
+        ViewColor.backgroundColor = UIColor.black
+        }else{
+            Slider2.value = 0
+            Slider2.isEnabled = false
+        }}
     
     @IBAction func blueSwitch(_ sender: UISwitch) {
-        Slider3.isEnabled = false
+        if blueSwitch.isOn {
+        Slider3.isEnabled = true
         Slider3.value = 0
+        ViewColor.backgroundColor = UIColor .black
         
-    }
+        }else {
+            Slider3.value = 0
+            Slider3.isEnabled = false
+        } }
     
 }
     
